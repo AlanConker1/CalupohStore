f6bd2e36b7ed5c965a4699ac54333b29eacf18f5
AlanConker1 <79279610+AlanConker1@users.noreply.github.com>
2021-03-16 18:16:30 -0600

    Archivos calupoh

---
+++ src/js/app.js
@@ -0,0 +1,35 @@
+/*
+document.addEventListener('DOMContentLoaded', function() {
+    scrollNav();
+    navegacionFija();
+});
+
+function navegacionFija() {
+    const barra = document.querySelector('.header');
+    //Registrar el intersection observer
+    const observer = new IntersectionObserver(function(entries) {
+        if (entries[0].isIntersecting) {
+            barra.classList.remove('fijo');
+        } else {
+            barra.classList.add('fijo');
+        }
+    });
+    //Elemento para observar
+    observer.observe(document.querySelector('.sobre-nosotros'));
+}
+
+function scrollNav() {
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+
+    enlaces.forEach(function(enlace) {
+        enlace.addEventListener('click', function(e) {
+            e.preventDefault();
+            const seccion = document.querySelector(e.target.attributes.href.value);
+
+            seccion.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+}
+*/
